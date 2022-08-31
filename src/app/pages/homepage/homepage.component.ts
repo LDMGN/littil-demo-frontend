443512c6b40d34c8a09b2c0bfcef3af8409e9647
Anja van Hagen <anja.vanhagen@ns.nl>
2022-08-31 19:55:44 +0200

    LoginModal only for logging in

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
 import { ModalController } from '../../components/modal/modal.controller';
-import {
-  LoginModalComponent,
-  LoginType,
-} from '../login-modal/login-modal.component';
+import { LoginModalComponent } from '../login-modal/login-modal.component';
 
 @Component({
   selector: 'littil-homepage',
   templateUrl: './homepage.component.html',
 })
 export class HomepageComponent {
-  LoginType = LoginType;
-
   constructor(private modalController: ModalController) {}
 
-  public openLoginModal(type: LoginType) {
-    return this.modalController.present(LoginModalComponent, { type: type });
+  public openLoginModal() {
+    return this.modalController.present(LoginModalComponent);
   }
 }
--- src/app/pages/login-modal/login-modal.component.spec.ts
+++ src/app/pages/login-modal/login-modal.component.spec.ts
@@ -5,32 +5,22 @@ import { MockComponent } from 'ng-mocks';
 import { ButtonComponent } from '../../components/button/button.component';
 import { FormErrorMessageComponent } from '../../components/forms/form-error-message/form-error-message.component';
 import { FormInputPasswordComponent } from '../../components/forms/password-input/form-input-password.component';
-import { FormInputRadioComponent } from '../../components/forms/radio-input/form-input-radio.component';
 import { FormInputTextComponent } from '../../components/forms/text-input/form-input-text.component';
-import { TeacherService } from '../../services/teacher/teacher.service';
-import {
-  ILoginModalInput,
-  LoginModalComponent,
-  LoginType,
-} from './login-modal.component';
+import { LoginModalComponent } from './login-modal.component';
 
 describe('LoginModalComponent', () => {
   let spectator: Spectator<LoginModalComponent>;
   let closeSpy: jest.SpyInstance;
-  let loginSpy: jest.SpyInstance;
-  let registerTeacherSpy: jest.SpyInstance;
-  let registerSchoolSpy: jest.SpyInstance;
 
   const createComponent = createComponentFactory({
     component: LoginModalComponent,
     declarations: [
       MockComponent(FormInputTextComponent),
       MockComponent(FormInputPasswordComponent),
-      MockComponent(FormInputRadioComponent),
       MockComponent(FormErrorMessageComponent),
       MockComponent(ButtonComponent),
     ],
-    mocks: [TeacherService],
+    mocks: [],
   });
 
   beforeEach(() => {
@@ -41,174 +31,38 @@ describe('LoginModalComponent', () => {
     spectator.detectChanges();
     spectator.component.close = () => undefined;
     closeSpy = jest.spyOn(spectator.component, 'close');
-    loginSpy = jest.spyOn(spectator.component, 'login');
-    registerTeacherSpy = jest.spyOn(spectator.component, 'registerTeacher');
-    registerSchoolSpy = jest.spyOn(spectator.component, 'registerSchool');
   });
 
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
   });
 
-  describe('onOpen', () => {
-    it('should set type from data [Login]', () => {
-      spectator.component.onOpen({ type: LoginType.Login });
-      expect(spectator.component.type).toEqual(LoginType.Login);
-    });
-    it('should set type from data [Register]', () => {
-      spectator.component.onOpen({ type: LoginType.Register });
-      expect(spectator.component.type).toEqual(LoginType.Register);
-    });
-    it('should disable forms when data is undefined', () => {
-      spectator.component.onOpen({} as ILoginModalInput);
-      expect(spectator.component.loginForm.disabled).toBe(true);
-      expect(spectator.component.registerSchoolForm.disabled).toBe(true);
-      expect(spectator.component.registerTeacherForm.disabled).toBe(true);
-      expect(spectator.component.type).toBeUndefined();
-    });
-  });
-
-  describe('onRegisterTypeChanged', () => {
-    it('should set registerType when changed', () => {
-      spectator.component.onOpen({ type: LoginType.Login });
-      expect(spectator.component.registerType).toEqual('teacher');
-      spectator.component.onRegisterTypeChanged('school');
-      expect(spectator.component.registerType).toEqual('school');
-    });
-  });
-
-  describe('onClickOK', () => {
-    it('should call onClickLogin() when type [Login]', () => {
-      spectator.component.onOpen({ type: LoginType.Login });
-      spectator.component.onClickOK();
-      expect(loginSpy).toHaveBeenCalledTimes(1);
-      expect(registerTeacherSpy).toHaveBeenCalledTimes(0);
-      expect(registerSchoolSpy).toHaveBeenCalledTimes(0);
-    });
-
-    it('should call onClickRegisterTeacher() when type [Register] & registerType [teacher]', () => {
-      spectator.component.onOpen({ type: LoginType.Register });
-      spectator.component.registerType = 'teacher';
-      spectator.component.onClickOK();
-      expect(loginSpy).toHaveBeenCalledTimes(0);
-      expect(registerTeacherSpy).toHaveBeenCalledTimes(1);
-      expect(registerSchoolSpy).toHaveBeenCalledTimes(0);
-    });
-
-    it('should call onClickRegisterSchoolSpy() when type [Register] & registerType [school]', () => {
-      spectator.component.onOpen({ type: LoginType.Register });
-      spectator.component.registerType = 'school';
-      spectator.component.onClickOK();
-      expect(loginSpy).toHaveBeenCalledTimes(0);
-      expect(registerTeacherSpy).toHaveBeenCalledTimes(0);
-      expect(registerSchoolSpy).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('login', () => {
+  describe('onClickLogin', () => {
     it('should not close modal when form is invalid', async () => {
-      spectator.component.onOpen({ type: LoginType.Login });
-      await spectator.component.login();
+      await spectator.component.onClickLogin();
       expect(spectator.component.loginForm.invalid).toBe(true);
       expect(closeSpy).not.toHaveBeenCalled();
     });
 
     it('should close modal when form is valid', async () => {
-      spectator.component.onOpen({ type: LoginType.Login });
       spectator.component.loginForm.get('email')?.setValue('email@email.com');
       spectator.component.loginForm.get('password')?.setValue('123');
-      await spectator.component.login();
+      await spectator.component.onClickLogin();
       expect(spectator.component.loginForm.invalid).toBe(false);
-      expect(closeSpy).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('registerTeacher', () => {
-    it('should not close modal when form is invalid', async () => {
-      spectator.component.onOpen({ type: LoginType.Register });
-      await spectator.component.registerTeacher();
-      expect(spectator.component.registerTeacherForm.invalid).toBe(true);
-      expect(closeSpy).not.toHaveBeenCalled();
-    });
-
-    it('should not close modal when form has Invalid postalCode', async () => {
-      spectator.component.onOpen({ type: LoginType.Register });
-      spectator.component.registerTeacherForm
-        .get('postalCode')
-        ?.setValue('12-INVALID');
-      await spectator.component.registerTeacher();
-      expect(spectator.component.registerTeacherForm.invalid).toBe(true);
-      expect(closeSpy).not.toHaveBeenCalled();
-    });
-
-    it('should close modal when form is valid [register]', async () => {
-      spectator.component.onOpen({ type: LoginType.Login });
-      spectator.component.registerTeacherForm.setValue({
-        firstName: 'Firstname',
-        surname: 'Lastname',
-        email: 'email@gmail.nl',
-        postalCodeNumbers: 2172,
-        postalCodeLetters: 'BG',
-        postalCode: '2172BG',
-        country: 'Nederland',
-      });
-      await spectator.component.registerTeacher();
-      expect(spectator.component.registerTeacherForm.valid).toBe(true);
-      expect(closeSpy).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('registerSchool', () => {
-    it('should not close modal when form is invalid', async () => {
-      spectator.component.onOpen({ type: LoginType.Login });
-      await spectator.component.registerSchool();
-      expect(spectator.component.registerSchoolForm.invalid).toBe(true);
-      expect(closeSpy).not.toHaveBeenCalled();
-    });
-
-    it('should close modal when form is valid [login]', async () => {
-      spectator.component.onOpen({ type: LoginType.Login });
-      spectator.component.registerSchoolForm
-        .get('schoolName')
-        ?.setValue('Schoolname');
-      await spectator.component.registerSchool();
-      expect(spectator.component.registerSchoolForm.invalid).toBe(false);
       expect(closeSpy).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('onClickCancel', () => {
     it('should close modal when clicked on cancel', () => {
-      spectator.component.onOpen({ type: LoginType.Login });
       spectator.component.onClickCancel();
       expect(closeSpy).toHaveBeenCalledTimes(1);
     });
   });
 
   describe('Template', () => {
-    it('should show login form when type [Login]', () => {
-      spectator.component.onOpen({ type: LoginType.Login });
+    it('should show login form', () => {
       expect(spectator.query('[data-test="form-login"]')).toBeDefined();
-      expect(spectator.query('[data-test="form-register-teacher"]')).toBeNull();
-      expect(spectator.query('[data-test="form-register-school"]')).toBeNull();
-    });
-    it('should show registerTeacher form when type [Register] and registerType [teacher]', () => {
-      spectator.component.onOpen({ type: LoginType.Register });
-      spectator.component.registerType = 'teacher';
-      expect(spectator.query('[data-test="form-login"]')).toBeNull();
-      expect(
-        spectator.query('[data-test="form-register-teacher"]')
-      ).toBeDefined();
-      expect(spectator.query('[data-test="form-register-school"]')).toBeNull();
-    });
-    it('should show registerSchool form when type [Register] and registerType [school]', () => {
-      spectator.component.onOpen({ type: LoginType.Register });
-      spectator.component.registerType = 'school';
-      expect(spectator.query('[data-test="form-login"]')).toBeNull();
-      expect(spectator.query('[data-test="form-register-teacher"]')).toBeNull();
-      expect(
-        spectator.query('[data-test="form-register-school"]')
-      ).toBeDefined();
     });
   });
 });
