7b726fdfbf6bd53b136c95a0ec9224257168db08
Jack van den Berg <jackvdberg1992@gmail.com>
2022-09-10 19:51:13 +0200

    schools.component.ts generated and routing added

--- src/app/pages/website/info/information.module.ts
+++ src/app/pages/website/info/information.module.ts
@@ -3,17 +3,23 @@ import {CommonModule} from '@angular/common';
 import {ContentContainerModule} from "../../../components/content-container/content-container.module";
 import {RouterModule, Routes} from "@angular/router";
 import {InformationComponent} from "./information/information.component";
+import {SchoolsComponent} from './schools/schools.component';
 
 const routes: Routes = [
   {
     path: '',
     component: InformationComponent,
   },
+  {
+    path: 'schools',
+    component: SchoolsComponent,
+  },
 ];
 
 @NgModule({
   declarations: [
-    InformationComponent
+    InformationComponent,
+    SchoolsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/pages/website/info/schools/schools.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'littil-schools',
+  templateUrl: './schools.component.html',
+})
+export class SchoolsComponent {}
